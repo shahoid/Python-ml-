c48cfe024e36958aa5c07293ec323fe49e9e247e
Shahid Siddique <159997849+shahoid@users.noreply.github.com>
2024-10-15 08:47:48 -0700

    Create acc_balance.py

---
+++ acc_balance.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 14 08:35:58 2024
+
+@author: sapta
+"""
+
+class Account:
+    
+    def __init__(self,bal,acc_no):
+        self.balance=bal
+        self.account_no=acc_no
+    
+    def setData(self):
+        account_no=int(input("Enter account no:"))
+        balance=int(input("Enter the balance in your account:"))
+    
+    def Debit(self):
+        debit=int(input("Enter ammount to Debit :"))
+        self.balance-=debit
+        
+    def Credit(self):
+        credit=int(input("Enter ammount to credit :"))
+        self.balance+=credit
+        
+    def print(self):
+        print("Account no : ",self.account_no)
+        print("Balance left in your account : ",self.balance)
+        
+acc1=Account(10000,8888)
+acc1.Debit()
+acc1.Credit()
+acc1.print()
